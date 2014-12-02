f81c90f1da7ee11977af1cbb6c5705bb89164aad
Sertan Senturk <contact@sertansenturk.com>
2014-12-02 19:34:16 +0100

    Continue writing documentation for the main function

--- phraseSeg.m
+++ phraseSeg.m
@@ -1,30 +1,63 @@
 function varargout = phraseSeg(varargin)
 % PHRASESEG Automatic phrase segmentation on scores of Ottoman-Turkish
 % makam music
-%   This function 
+%   This is a wrapper to call the main steps in automatic phrase
+%   segmentation. It allows the user to call the corresponding functions
+%   without specifying the location of the usul and note dictionaries. The
+%   user simply has to specify the input file/folder for each step and the
+%   output file/folders if desired. 
 %
-% Test run ........................ test
-%       Inputs: none
-%       Outputs: none
-% Get manual segments in SymbTr: .. getSegments (scoreFolder/scoreFile) [outFolder/outFile]
-%       Inputs:
+%   Example call:
+%       phraseSeg('segment', testFolder, FLDmodelFile, outFolder)
+%
+%   If you are call the standalone binary provided in the package from the
+%   terminal, append the run_mcr.sh function and with the Command syntax
+%   (http://www.mathworks.com/help/matlab/ref/syntax.html)
+%
+%       run_mcr.sh phraseSeg segment testFolder FLDmodelFile outFolder
+%
+% learnBoundStat .................. Learn boundary statistics 
+%       Inputs: 
+%           noteTableFile
+%           [outFile]
 %       Outputs:
-% Learn boundary statistics ....... learnBoundStat noteTableFile [outFile
-%       Inputs:
+% extractFeature .................. Feature Extraction
+%       Inputs: 
+%           (scoreFolder/scoreFile)
+%           boundStatFile
+%           [outFolder/outFile]
 %       Outputs:
-% Feature Extraction............... extractFeature (scoreFolder/scoreFile) boundStat_file [outFolder/outFile]
-%       Inputs:
+% train ........................... Training
+%       Inputs: 
+%           trainingFolder
+%           [outFolder]
 %       Outputs:
-% Training ........................ train trainingFolder [outFolder]
+% segment ......................... Segmentation
 %       Inputs:
+%           (testFolder/testFile)
+%           FLDmodelFile
+%           [outFolder/outFile]
 %       Outputs:
-% Segmentation .................... segment (testFolder/testFile) FLDmodel_file [outFolder/outFile]
+% evaluate ........................ Evaluation
 %       Inputs:
+%           [evaluateFolder/evaluateFile]
+%           [plotROC]
 %       Outputs:
-% Evaluation ...................... evaluate [evaluateFolder/evaluateFile] [plotROC]
-%       Inputs:
+%
+% There are also some additional functions:
+% test ............................ Test run 
+%       It can be used to check whether the wrapper works without any
+%       problems.
+%       Inputs: 
+%           - none -
+%       Outputs: 
+%           - none (will display success message on completion) -
+% getSegments ..................... Get manual segments in SymbTr
+%       Inputs: 
+%           (scoreFolder/scoreFile)
+%           [outFolder/outFile]
 %       Outputs:
-%    
+%  
 %   Sertan Senturk, 2 December 2012
 %   Universitat Pompeu Fabra
 %   email: sertan.senturk@upf.edu 
@@ -106,11 +139,11 @@ end
 if inputErr
     errstr = ['Wrong inputs! Usage: \n' ...
         'Test run ........................ "test" \n'...
-        'Get manual segments in SymbTr: .. "getSegments (scoreFolder/scoreFile) [outFolder/outFile]" \n'...
+        'Get manual segments in SymbTr ... "getSegments (scoreFolder/scoreFile) [outFolder/outFile]" \n'...
         'Learn boundary statistics ....... "learnBoundStat noteTableFile [outFile]" \n'...
-        'Feature Extraction............... "extractFeature (scoreFolder/scoreFile) boundStat_file [outFolder/outFile]" \n'...
+        'Feature Extraction............... "extractFeature (scoreFolder/scoreFile) boundStatFile [outFolder/outFile]" \n'...
         'Training ........................ "train trainingFolder [outFolder]" \n'...
-        'Segmentation .................... "segment (testFolder/testFile) FLDmodel_file [outFolder/outFile]" \n'...
+        'Segmentation .................... "segment (testFolder/testFile) FLDmodelFile [outFolder/outFile]" \n'...
         'Evaluation ...................... "evaluate [evaluateFolder/evaluateFile] [plotROC] "'];
     error('makamSymPhraseSeg:input', errstr)
 end
