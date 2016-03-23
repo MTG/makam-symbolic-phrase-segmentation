5c61ba77cce8c8cf26dacfcb7be5e5668ff04975
Sertan Senturk <contact@sertansenturk.com>
2016-03-24 00:01:49 +0100

    Separated the demo for linux bash

--- phraseSeg_mcr_demo.sh
+++
@@ -1,3 +0,0 @@
-./run_mcr.sh ./phraseSeg trainWrapper ./sampleData/trainFiles.json ./sampleData/boundStat.mat ./sampleData/FLDmodelFile
-
-./run_mcr.sh ./phraseSeg segmentWrapper ./sampleData/boundStat.mat ./sampleData/FLDmodelFile.mat ./sampleData/testFile.json ./sampleData/tmp/seg.json
